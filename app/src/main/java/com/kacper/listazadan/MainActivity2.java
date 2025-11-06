7df9464413cdf91a706a4f31cac9f3f14cb1e934
ScislakKacper <kacpersci7@gmail.com>
2025-11-06 22:22:14 +0100

    Wersja 2: pobranie elementow formularza, utworzenie ArrayAdaptera

--- app/src/main/java/com/kacper/listazadan/MainActivity2.java
+++ app/src/main/java/com/kacper/listazadan/MainActivity2.java
@@ -16,16 +16,29 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private EditText nazwa_produktu;
+    private EditText cena_produktu;
+    private EditText opis_produktu;
+    private Button przycisk_dodaj;
+    private TextView suma_ceny;
+    private ListView lista_produktow;
+    private ArrayList<Produkt> produkty_lista;
+    private ArrayAdapter<Produkt> arrayAdapter;
+    float cena = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main2); // wazne - ustala layout
+        // java - camelcase, xml - snakecase
+        nazwa_produktu = findViewById(R.id.nazwa_produktu);
+        cena_produktu = findViewById(R.id.cena_produktu);
+        opis_produktu = findViewById(R.id.opis_produktu);
+        przycisk_dodaj = findViewById(R.id.dodaj);
+        suma_ceny = findViewById(R.id.suma_ceny);
+        lista_produktow = findViewById(R.id.lista_produktow);
+        produkty_lista = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, produkty_lista);
+        lista_produktow.setAdapter(arrayAdapter);
     }
 }
