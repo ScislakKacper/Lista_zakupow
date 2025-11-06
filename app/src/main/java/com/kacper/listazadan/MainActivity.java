1b198f04a455756ed3f77aafd8f76f9a345bc197
ScislakKacper <kacpersci7@gmail.com>
2025-11-06 22:08:25 +0100

    Wersja 1: pobranie elementow formularza, definicja ArrayAdaptera

--- app/src/main/java/com/kacper/listazadan/MainActivity.java
+++ app/src/main/java/com/kacper/listazadan/MainActivity.java
@@ -1,6 +1,10 @@
 package com.kacper.listazadan;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private Button przyciskDodaj;
+    private EditText produkt;
+    private ListView listaZakupow;
+    private ArrayList<String> listaProduktow;
+    private ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        przyciskDodaj = findViewById(R.id.dodajProdukt);
+        produkt = findViewById(R.id.produkt);
+        listaZakupow = findViewById(R.id.listaZakupow);
+        listaProduktow = new ArrayList<>();
+        listaProduktow.add("Mleko");
+        listaProduktow.add("Woda");
+        listaProduktow.add("Sok");
+        arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, listaProduktow);
+        listaZakupow.setAdapter(arrayAdapter);
     }
 }
